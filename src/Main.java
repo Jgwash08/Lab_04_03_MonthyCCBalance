85fd157380cd400c6e49ecca43aaed7a9ae4f6a5
jgwash08 <washij2@mail.uc.edu>
2026-09-24 22:06:13 -0400

    Monthly CC Balance

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     void main() {
-        //Declarations
+        //Declaration
         double cardBalance = 5000.00;
         double interestRate = 0.17;
         double monthOneInterest = cardBalance * interestRate;
