243aed753787737fb6ab8c78f9cfef11e9ba8e4e
jgwash08 <washij2@mail.uc.edu>
2026-09-24 22:04:51 -0400

    Monthly CC Balance

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    void main() {
+        //Declarations
+        double cardBalance = 5000.00;
+        double interestRate = 0.17;
+        double monthOneInterest = cardBalance * interestRate;
+
+        cardBalance = cardBalance + monthOneInterest;
+        // Display
+        System.out.println("Initial Credit Card Balance: $5000.0");
+        System.out.println("Interest due after 1 month:" + monthOneInterest);
+
+
+
+
+    }
+}
